5907f0de0567624525377006584965c0357d219d
Aakash Deep <82133913+AAKASHDEEP786@users.noreply.github.com>
2025-09-17 21:06:11 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.skyopsx.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.skyopsx.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.skyopsx_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.skyopsx_subnet[*].id
 }
 
